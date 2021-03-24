0bcef8eba52f1fd64c2e8a55fb0130b720650126
edem1 <mohan.ede@in.pega.com>
2021-03-25 00:40:53 +0530

    Changes in appspec.yml

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,4 +4,4 @@
 crontab -l | { cat; echo "@reboot pm2 start /var/www/server.js -i 0 --name \"node-app\""; } | crontab -
 sudo pm2 stop node-app
 # actually start the server
-sudo pm2 start /var/www/scripts/server.js -i 0 --name "node-app"
+sudo pm2 start /var/www/counter/scripts/server.js -i 0 --name "node-app"pwd
