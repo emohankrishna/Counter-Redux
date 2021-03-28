7221ca030ce8908045f237ee244ca4b0acd524d2
edem1 <mohan.ede@in.pega.com>
2021-03-28 20:48:04 +0530

    Test Deploy

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Counter from "../src/containers/Counter"
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Counter />, div);
 });
