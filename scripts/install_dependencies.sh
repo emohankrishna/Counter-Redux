4e56b68ce3efae2be64d5a722238a78c5a5671fd
edem1 <mohan.ede@in.pega.com>
2021-03-29 18:33:59 +0530

    Changes in yml

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,8 +8,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 node -e "console.log('Running Node.js ' + process.version)"
 cd home/ec2-user/counter
-sudo yum init -y
-yum install -y express
+echo "Before Initilizing package.json"
+npm init -y
+echo "Before Installing express"
+npm install -y express
 # install pm2 to restart node app
+echo "Before Installing pm2"
 npm install pm2@latest -g
 pm2 update
