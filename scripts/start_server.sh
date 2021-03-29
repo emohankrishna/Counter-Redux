ba28a60565f68c19db804b4feecfb96ea981ac68
edem1 <mohan.ede@in.pega.com>
2021-03-29 10:57:14 +0530

    Changes in yml

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,6 @@
 
 # this will restart app/server on instance reboot
 crontab -l | { cat; echo "@reboot pm2 start /home/ec2-user/counter/scripts/server.js"; } | crontab -
-pm2 stop node-app
+sudo pm2 stop node-app
 # actually start the server
-pm2 start /home/ec2-user/counter/scripts/server.js --name node-app
+sudo pm2 start /home/ec2-user/counter/scripts/server.js --name node-app
