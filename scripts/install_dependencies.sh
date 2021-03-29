f96470c1a3979ce82c2b4e0e410134852877c3ea
edem1 <mohan.ede@in.pega.com>
2021-03-29 09:28:36 +0530

    Changes in yml

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,9 +6,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 node -e "console.log('Running Node.js ' + process.version)"
-sudo yum init -y
+npm init -y
 # install node and npm in one line
-yum install -y nodejs
-yum install -y express
+npm install -y express
 # install pm2 to restart node app
 npm i -g pm2
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,6 @@
 # sudo chmod 755 /var/www/server.js # optional
 # this will restart app/server on instance reboot
 crontab -l | { cat; echo "@reboot pm2 start /home/ec2-user/counter/scripts/server.js"; } | crontab -
-sudo pm2 stop node-app
+pm2 stop node-app
 # actually start the server
-sudo pm2 start /home/ec2-user/counter/scripts/server.js --name node-app
+pm2 start /home/ec2-user/counter/scripts/server.js --name node-app
