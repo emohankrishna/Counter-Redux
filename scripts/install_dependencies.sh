662f8911a4f3b273ae6a3cfac7603bfee4b07330
edem1 <mohan.ede@in.pega.com>
2021-03-29 13:54:23 +0530

    Changes in yml

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # update yum just in case
 npm init -y
 # install node and npm in one line
